91baa692b4cc556cb22e546a3237b174db88d6c4
jorge <jorgecarvajal1999@gmail.com>
2023-09-13 11:54:54 -0230

    stagged changues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  },
     // output: 'static',
   outDir: './docs',
   // site: 'https://jcarvajal1987.github.io',
