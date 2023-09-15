1c81c6116c03e3c34033c8d82bac4e89a651b814
jorge <jorgecarvajal1999@gmail.com>
2023-09-15 11:46:46 -0300

    fixdeploy2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   
-  image: {
+  image:   {
     remotePatterns: [{ protocol: "https" }],
   },
     // output: 'static',
