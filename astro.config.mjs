4926f56368fc0d1877c507d27334abb890499dd4
jorge <jorgecarvajal1999@gmail.com>
2023-09-15 09:02:27 -0300

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,13 +6,11 @@ export default defineConfig({
   image: {
     remotePatterns: [{ protocol: "https" }],
   },
-    output: 'server',
-    adapter: netlify(),
+    // output: 'static',
   outDir: './docs',
   // site: 'https://jcarvajal1987.github.io',
   // base:'/apr',
   build: {
-    client: './client',
     format: 'file',
     // assets: 'assets',
     assetsPrefix: '.'
