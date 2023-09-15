1710a9792e8324e38644244e046fbe39fdef9ee6
jorge <jorgecarvajal1999@gmail.com>
2023-09-15 08:55:06 -0230

    ready

---
+++ .netlify/functions-internal/chunks/_id__a91472f4.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./prerender_f5a6d718.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/chunks/prerender_f5a6d718.mjs
@@ -0,0 +1,5 @@
+const noop = () => {};
+ export default noop;
+export const file = noop;
+export const getStaticPaths = noop;
+export const url = noop;
