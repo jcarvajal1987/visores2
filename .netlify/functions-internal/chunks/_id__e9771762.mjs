e76e83a8f97336850531a60678cabf752fa2299c
jorge <jorgecarvajal1999@gmail.com>
2023-09-16 15:28:21 -0300

    icon fix

---
+++ .netlify/functions-internal/chunks/_id__e9771762.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/_id__34957442.mjs').then(n => n._);
+
+export { page };
--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,12 +1,12 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_9da03411.mjs';
+import { manifest } from './manifest_fc9df558.mjs';
 import './chunks/astro_f59ab796.mjs';
-import './chunks/pages/image-endpoint_9a362001.mjs';
-import './chunks/pages/_id__13f86cf5.mjs';
+import './chunks/pages/image-endpoint_dfd9baeb.mjs';
+import './chunks/pages/_id__34957442.mjs';
 /* empty css                                */
-const _page0  = () => import('./chunks/image-endpoint_fa0c82f6.mjs');
-const _page1  = () => import('./chunks/_id__6a840c32.mjs');const pageMap = new Map([["node_modules/.pnpm/astro@3.0.8/node_modules/astro/dist/assets/image-endpoint.js", _page0],["src/pages/[id].astro", _page1]]);
+const _page0  = () => import('./chunks/image-endpoint_f2c0b6c7.mjs');
+const _page1  = () => import('./chunks/_id__e9771762.mjs');const pageMap = new Map([["node_modules/.pnpm/astro@3.0.8/node_modules/astro/dist/assets/image-endpoint.js", _page0],["src/pages/[id].astro", _page1]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
